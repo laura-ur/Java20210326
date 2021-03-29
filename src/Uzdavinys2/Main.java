ce74ba16f8341a8c16c9e759f5f069d22b3065d6
laura-ur <laurai.urnikytei@gmail.com>
2021-03-29 21:20:38 +0300

    second done

--- src/Uzdavinys2/Main.java
+++ src/Uzdavinys2/Main.java
@@ -10,7 +10,7 @@ public class Main {
                 int result = i * two;
                 int result1 = i * three;
                 int result2 = i * four;
-                System.out.printf("%-15.12s %-15.12s %-15.12s%n", two + " * " + i + " = " + result, +three + " * " + i + " = " + result1, four + " * " + i + " = " + result2);
+                System.out.printf("%-15.12s %-15.12s %-12s%n", two+" * "+i+" = "+result, three+" * "+i+" = "+result1, four+ " * "+i+" = "+result2);
             }
         }
         System.out.println(" ");
@@ -23,7 +23,7 @@ public class Main {
                 int result = i * five;
                 int result1 = i * six;
                 int result2 = i * seven;
-                System.out.printf("%-15.12s %-15.12s %-15.12s%n", five + " * " + i + " = " + result, +six + " * " + i + " = " + result1, seven + " * " + i + " = " + result2);
+                System.out.printf("%-15.12s %-15.12s %-12s%n", five+" * "+i+" = "+result, six+" * "+i+" = "+result1, seven+" * "+i+" = "+result2);
             }
         }
         System.out.println(" ");
@@ -34,7 +34,7 @@ public class Main {
                 int nine = 9;
                 int result = i * eight;
                 int result1 = i * nine;
-                System.out.printf("%-15.12s %-15.12s%n", eight + " * " + i + " = " + result, +nine + " * " + i + " = " + result1);
+                System.out.printf("%-15.12s %-12s%n", eight+" * "+i+" = "+result, nine+" * "+i+ " = "+result1);
             }
 
         }
